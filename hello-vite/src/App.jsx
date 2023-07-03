61b2555f3d33d8413e4d2498c836249d1aa94e10
israelusman234 <usmanyisrael@gmail.com>
2023-07-03 23:20:52 +0100

    adding new files

---
+++ hello-vite/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import gituser from 'gituser.json';
+const App = () =>{
+  return (
+    <>
+      <div>
+       <h1></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ hello-vite/src/component/counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+<div>
+        <h2>0</h2>
+   <div>
+    <button>+</button>
+    <button>-</button>
+    </div>
+</div>
+  )
+}
+
+export default counter
